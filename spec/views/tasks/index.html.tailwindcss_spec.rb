7f80ec27373712ba075765188b35c337e6782bd5
nayu <19748682+nayu7u@users.noreply.github.com>
2024-09-28 17:48:53 +0000

    feat: rails g scaffold task

---
+++ spec/views/tasks/index.html.tailwindcss_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/index", type: :view do
+  before(:each) do
+    assign(:tasks, [
+      Task.create!(),
+      Task.create!()
+    ])
+  end
+
+  it "renders a list of tasks" do
+    render
+    cell_selector = 'div>p'
+  end
+end
